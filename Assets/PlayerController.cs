5e7a8430ef2d679400523136b63bd2974aa9b703
Martin Parm <parmus@gmail.com>
2018-10-13 17:18:34 +0200

    Sketched out initial first scene

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	[SerializeField] CameraController CurrentCamera;
+
+	// Update is called once per frame
+	void Update () {
+		CurrentCamera.Rotate(Input.GetAxis("Horizontal"), -Input.GetAxis("Vertical"));
+
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit)){
+			CameraController nextCamera = hit.transform.GetComponent<CameraController>();
+			if (nextCamera != null) {
+				if (Input.GetMouseButton(0)) {
+					nextCamera.MakeActive();
+					CurrentCamera = nextCamera;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	[SerializeField] Transform CameraRotation;
+	[SerializeField] Transform CameraAttachment;
+	[SerializeField] float MinHorizontalAngle; 
+	[SerializeField] float MaxHorizontalAngle;
+	[SerializeField] float MinVerticalAngle;
+	[SerializeField] float MaxVerticalAngle;
+
+	[SerializeField][Range(10f, 50f)] float RotationSpeed = 30f;
+
+	private float currentHorizonalRotation;
+	private float currentVerticalRotation;
+
+	void Start() {
+		currentHorizonalRotation = CameraRotation.localEulerAngles.y;
+		currentVerticalRotation = CameraRotation.localEulerAngles.x;
+	}
+
+	public void MakeActive(){
+		Transform CameraTransform = Camera.main.transform;
+		CameraTransform.parent = CameraAttachment;
+		CameraTransform.localPosition = Vector3.zero;
+		CameraTransform.localRotation = Quaternion.identity;
+	}
+
+	public void Rotate(float h, float v) {
+		currentHorizonalRotation = Mathf.Clamp(currentHorizonalRotation + h*Time.deltaTime*RotationSpeed, MinHorizontalAngle, MaxHorizontalAngle);
+		currentVerticalRotation = Mathf.Clamp(currentVerticalRotation + v*Time.deltaTime*RotationSpeed, MinVerticalAngle, MaxVerticalAngle);
+
+		CameraRotation.localRotation = Quaternion.Euler(currentVerticalRotation, currentHorizonalRotation, 0f);
+	}
+
+
+}
